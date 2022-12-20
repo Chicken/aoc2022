715da5951f3d11e82095284b066a877e83df1c46
Antti <antti@antti.codes>
2022-12-20 17:08:01 +0200

    day20

---
+++ day20/part2.ts
@@ -0,0 +1,19 @@
+const input = Deno.readTextFileSync("input").trim().split("\n").map(Number).map((e) => e * 811589153).map((
+    v,
+    i,
+) => [v, i]);
+
+for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < input.length; i++) {
+        const index = input.findIndex((e) => e[1] === i);
+        const [[value, originalIndex]] = input.splice(index, 1);
+        const newIndex = (index + value + input.length) % input.length;
+        input.splice(newIndex === 0 ? input.length : newIndex, 0, [value, originalIndex]);
+    }
+}
+const indexZero = input.findIndex((e) => e[0] === 0);
+console.log(
+    input[(indexZero + 1000) % input.length][0] +
+        input[(indexZero + 2000) % input.length][0] +
+        input[(indexZero + 3000) % input.length][0],
+);
